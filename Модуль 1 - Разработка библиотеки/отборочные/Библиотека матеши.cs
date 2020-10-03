2643377f76c4f7ae91e8fa6d06b67b220a66f0d8
Diadema <danilka_maksimov_02@mail.ru>
2020-10-03 12:51:03 +0700

    Разработка библиотеки
    Разработка библиотеки по работе с математикой

---
+++ Модуль 1 - Разработка библиотеки/отборочные/Библиотека матеши.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace отборочные
+{
+    public static class БиблиотекаМатеши
+    {
+        public static double Сумма(double chislo1, double chislo2) 
+        {
+            return chislo1 + chislo2;
+        }
+        public static double Разность(double chislo1, double chislo2)
+        {
+            return chislo1 - chislo2;
+        }
+        public static double Умножение(double chislo1, double chislo2)
+        {
+            return chislo1 * chislo2;
+        }
+        public static double Деление(double chislo1, double chislo2)
+        {
+            return chislo1 / chislo2;
+        }
+        public static double ПлощадьКруга(double R)
+        {
+            return Math.PI * Math.Pow(R, 2);
+        }
+        public static double ПлощадьКвадрата(double P)
+        {
+            return P * 4;
+        }
+        public static double ПлощадьПараллелограмма(double a, double h )
+        {
+            return a * h;
+        }
+    }
+}
